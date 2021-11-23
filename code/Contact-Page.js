2b9a30ffa6ebeeabfe0b3f3be12151c868e93b61
TSUNODA <sami.lebnouj95500@gmail.com>
2021-11-23 12:14:12 +0100

    finish

--- code/Contact-Page.js
+++ code/Contact-Page.js
@@ -1,12 +1,18 @@
 function openModal () {
-    document.getElementById("WinContact").style;
+    document.getElementById("WinContact").style.top = "200px";
     var test = document.getElementById("MessageCase");
-    var MessageIn = document.getElementById("MessageInside"); 
-      
+    var MessageIn = document.getElementById("MessageInside");   
+
     MessageIn.innerHTML = "Votre message a été envoyé avec succès :  "  + test.value;
+
+    
 
     setTimeout(function() {
         WinContact.style.display = 'block';
     },200);
 }
 onclick.classList.remove("openModal");
+
+function closeModal() {
+    document.getElementById("WinContact").style.top = "-400px";
+}
